f630726184547273d25acd0cf4cc5817987b3dbc
Ben David Le <ben@getacademy.no>
2022-05-13 11:11:24 +0200

    Add project files.

---
+++ BossFight/GameCharacter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BossFight
+{
+    internal class GameCharacter
+    {
+        //Properties
+        public int Health;
+        public int Strength;
+        public int Stamina;
+        private int _maxStamina;
+
+        //Constructor ctor
+        //var Player = new GameCharacter(100, 20, 40)
+        public GameCharacter(int health, int strength, int stamina)
+        {
+            Health = health;
+            Strength = strength;
+            Stamina = stamina;
+            _maxStamina = stamina;
+        }
+
+        //Metoder
+        public void Fight(int str)
+        {
+            if (Stamina == 0)
+            {
+                Recharge();
+            }
+            else
+            {
+                Health -= str;
+                Stamina -= 10;
+            }
+
+
+        }
+        public void Recharge()
+        {
+            //stamina = maxstamina
+            Stamina = _maxStamina;
+        }
+
+    }
+
+}
---
+++ BossFight/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BossFight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Boss = new GameCharacter(400, 30, 10);
+            var Hero = new GameCharacter(100, 20, 40);
+
+            Random rnd = new Random();
+
+
+            while (Boss.Health > 0)
+            {
+                bool isPlayer = true;
+                var playerName = isPlayer ? "Hero" : "Boss";
+                var player = isPlayer ? Hero : Boss;
+                int dmg = rnd.Next(0, player.Strength + 1);
+
+                player.Fight(dmg);
+                Console.WriteLine($"{playerName} hit {playerName} with {dmg} damage, {playerName} has {player.Health} left.");
+                isPlayer = !isPlayer;
+            }
+
+
+        }
+    }
+}
